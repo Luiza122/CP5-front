4015ea10eac3a1cfd44ee6bdcdcaaa79a7a71fa4
Luiza Macena Dantas <luiza.macena2013@gmail.com>
2024-10-02 23:06:04 -0300

    .

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,9 @@
 
 $(document).ready(function() {
-    // Função para exibir um alerta quando o formulário é enviado
+   
     $('form').on('submit', function(event) {
-        event.preventDefault(); // Evita o envio padrão do formulário
+        event.preventDefault(); 
         alert('Obrigado por entrar em contato! Responderemos em breve.');
-        $(this).trigger('reset'); // Limpa o formulário após o envio
+        $(this).trigger('reset'); 
     });
 });
